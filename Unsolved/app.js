4096c3eb1153c61e6804a3a21096a384f3922c44
Dessalinesagginie <desagginie@gmail.com>
2024-09-26 09:31:14 -0500

    wild search2

--- Unsolved/app.js
+++ Unsolved/app.js
@@ -12,6 +12,60 @@
 // and display it on the screen in the same order.
 
 // For example, the sentence "This is an example"
+
+// would become `siht si na elpmaxe.`
+
+// "We ran"
+
+// "eW nar"
+
+const words = ["We", "ran"];
+for (let i = words.length - 1; i >= 0; i--) {
+    console.log(words[i]);
+}
+
+// const str = "Hello";
+// let reversedStr = '';
+// for (let i = str.length - 1; i >= 0; i++) {
+//     reversedStr += str[i];
+// }
+// console.log(reversedStr); // Output: "olleH"
+
+// If the word is a palindrome, have it display in red.
+
+
+// Notes:
+// ======
+
+// Remove periods.
+// Add the result to the page in the div with the id "Result".
+// Here are some test phrases to put in your JavaScript:
+// "Otto bought a racecar he cannot afford."
+// "Otto lost the deed to his house."
+// "Otto needs Xanax."
+
+
+// Bonus:
+// ======
+
+// Remove all "special characters" (commas, periods, etc).
+// Do not use the reverse() method.
+
+
+// Reverse each word in a sentence
+
+
+// Create an Array
+const sentence = ["This is an example"];
+// elpmaxe na si siht
+
+var word = "This";
+for (var i = 0; i < word.length; i--) {
+console.log("My name contains the letter " + word [i] + ".");
+}
+// Reverse the Array
+
+// For example, the sentence "This is an example"
 // would become `siht si na elpmaxe.`
 
 // If the word is a palindrome, have it display in red.
