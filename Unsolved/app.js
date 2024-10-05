b5ae5cf75230a1bae3991517330fcc88627b88a1
Dessalinesagginie <desagginie@gmail.com>
2024-10-04 21:49:19 -0500

    same ol nar ew

--- Unsolved/app.js
+++ Unsolved/app.js
@@ -1,19 +1,3 @@
-// Reverse in Place w/Palindrome Detector
-
-
-// Instructions
-// ============
-
-// A palindrome is a sentence that reads the same backwards and forwards:
-// "Was it a car or a cat I saw?"
-
-// Reverse each word in a sentence
-
-// and display it on the screen in the same order.
-
-// For example, the sentence "This is an example"
-
-// would become `siht si na elpmaxe.`
 
 var str ="we ran" 
 var strSplit = str.split(" ") 
@@ -25,60 +9,14 @@ words.push(strSplit[1])
 console.log(words.pop()) 
 console.log(words.pop())
 
-for (var i = words.length-1; i >= 0; i--) { 
-    // document.getElementById("demo").innerHTML = words[i];
-    // console.log(words[0])
-    // console.log(words[1])
-    console.log(words[i])
-}
+words.push("n","a","r"," ")
+
+words.push("e","w")
 
 for (var i = 0; i < words.length; i++) { 
     console.log(words[i])
-
 }
-console.log (words [1])
-console.log(words[0]) 
-// Notes:
-// ======
 
-// Remove periods.
-// Add the result to the page in the div with the id "Result".
-// Here are some test phrases to put in your JavaScript:
-// "Otto bought a racecar he cannot afford."
-// "Otto lost the deed to his house."
-// "Otto needs Xanax."
-
-
-// Bonus:
-// ======
-
-// Remove all "special characters" (commas, periods, etc).
-// Do not use the reverse() method.
-
-// Instructions
-// A palindrome is a sentence that reads the same backwards and forwards: "Was it a car or a cat I saw?"
-
-// Reverse each word in a sentence and display it on the screen in the same order. For example, the sentence This is an example would become siht si na elpmaxe. If the sentence is a palindrome, have it display in red.
-
-// NOTE:
-
-// Remove periods.
-
-// Add the result to the page in the div with the id "Result".
-
-// BONUS:
-
-// Remove all "special characters" (commas, periods, etc).
-
-// Do not use the reverse() method.
-
-// Here are some test phrases to put in your JavaScript:
-
-// "Otto bought a racecar he cannot afford."
-
-// "Otto lost the deed to his house."
-
-// "Otto needs Xanax."
-
+document.getElementById("demo").innerHTML=(words);
 
 
