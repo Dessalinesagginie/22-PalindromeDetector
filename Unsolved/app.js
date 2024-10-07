42ae7c885b1053ee347f858a5605f15afc50eccf
Dessalinesagginie <desagginie@gmail.com>
2024-10-07 14:15:18 -0500

    ?Progressional? Looping

--- Unsolved/app.js
+++ Unsolved/app.js
@@ -1,33 +1,54 @@
 
-var str = "we ran" 
-var strSplit = str.split(" ") 
-var words = [] 
+var str = "we ran" //string
+var strSplit = str.split(" ") //splits string items: we[0]; ran[1]
+var words = [] //creates empty array called Words
 
-words.push(strSplit[0]) 
-words.push(strSplit[1]) 
+words.push(strSplit[0]) //pushes we[0] into Words array 
+words.push(strSplit[1]) //pushes ran[1] into Words array
 
-console.log(words.pop()) 
-console.log(words.pop())
+// console.log items popped into words[] 
+var word = words.pop()
+var word = word.length
 
-//push reverse of words = we ran, into words []
-words.push("nar")
-words.push("ew")
-console.log(words)
+//Reverse words2
 
-// reverse the string - "nar"
- 
-var word1 = ["nar"]
+for (var i = word.length-1; i >= 0; i--) { 
+    console.log(word[i])
+}
+// ran  r
+// ran  a
+// ran  n
 
-// var word1Split = word1.split(" ") 
-var let = []
+var sent = "Otto needs Xanax."
 
-console.log(let.pop())
-console.log(let.pop())
-
-var myNames = ["Nicholas", "Andrew", "Maxwell", "Morgan"]
-var my4Names = myNames.join(" ") // "Nicholas Andrew Maxwell Morgan"
-console.log(my4Names); // "Nicholas Andrew Maxwell Morgan"
+for (var i = sent.length-1; i >= 0; i--) { 
+    console.log(sent[i])
+}
 
 
 
 
+
+
+
+
+
+
+
+
+
+// var myNames = ["Nicholas", "Andrew", "Maxwell", "Morgan"]
+// var my4Names = myNames.join(" ") // "Nicholas Andrew Maxwell Morgan"
+// console.log(my4Names); // "Nicholas Andrew Maxwell Morgan"
+
+// var str2 ="ran we"
+// var str2Split = str2.split(" ")
+// var words2 = []
+
+// words2.push(str2Split[0]) //pushes we[0] into Words array 
+// words2.push(str2Split[1])
+
+// for (var i = 0; i < words2.length; i++) { 
+//     console.log(words2[i])
+// }
+// console.log(words2)
